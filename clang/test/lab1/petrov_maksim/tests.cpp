a9e7ac51c4546284985525f2e8424d8d603b6f39
Maksim <redbulprofi@gmail.com>
2024-03-17 21:36:49 +0300

    fix name function

--- clang/test/lab1/petrov_maksim/tests.cpp
+++ clang/test/lab1/petrov_maksim/tests.cpp
@@ -1,13 +1,13 @@
 // RUN: %clang -cc1 -load %llvmshlibdir/WarningDeprecatedPlugin%pluginext -plugin WarningDeprecatedPlugin %s 2>&1 | FileCheck %s
 
-// CHECK: warning: Function 'deprecated_foo1' contains 'deprecated' in its name
+// CHECK: warning: Function 'deprecatedFoo1' contains 'deprecated' in its name
 void deprecatedFoo1();
 
-// CHECK-NOT: warning: Function 'Deprecated_foo2' contains 'deprecated' in its name
+// CHECK-NOT: warning: Function 'DeprecatedFoo2' contains 'deprecated' in its name
 void DeprecatedFoo2();
 
 struct St {
-// CHECK: warning: Function 'Func_deprecated3' contains 'deprecated' in its name
+// CHECK: warning: Function 'deprecatedFunc3' contains 'deprecated' in its name
 	void deprecatedFunc3() {}
 };
 
